8ebe7085a9a6da917c94ca1f38c1c598527fb349
anonylabo <139490975+anonylabo@users.noreply.github.com>
2024-05-28 03:22:56 -0500

    Update EODA.sh

--- scripts/Main/DC/EODA.sh
+++ scripts/Main/DC/EODA.sh
@@ -1,7 +1,7 @@
 
 python -u run.py \
   --path ./ \
-  --model GTFormer \
+  --model EODA \
   --sample_time '60min' \
   --itrs 10 \
   --train_epochs 50 \
@@ -18,7 +18,7 @@ python -u run.py \
 
 python -u run.py \
   --path ./ \
-  --model GTFormer \
+  --model EODA \
   --sample_time '45min' \
   --itrs 10 \
   --train_epochs 50 \
